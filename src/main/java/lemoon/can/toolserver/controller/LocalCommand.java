240c4c3d255000d874f054fb8f16015ad4ac60fa
Lemoon Can <lemooncan@LemoondeMacBook-Pro.local>
2023-12-06 14:34:47 +0800

    refactor: 💡 http请求打开本地文件
    1.限制只支持打开xmind文件 2.执行异常返回错误页面

--- src/main/java/lemoon/can/toolserver/controller/LocalCommand.java
+++ src/main/java/lemoon/can/toolserver/controller/LocalCommand.java
@@ -1,9 +1,11 @@
 package lemoon.can.toolserver.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import java.io.IOException;
 
 /**
@@ -11,33 +13,44 @@ import java.io.IOException;
  * @since 2023/12/4
  */
 @Controller
-@RequestMapping(value="/cmd")
+@RequestMapping(value = "/cmd")
 public class LocalCommand {
+    private final static String errorPage = "error";
+    private final static String closePage = "close";
 
     /**
      * 打开本地文件(通过执行open命令实现)
-     * @param path
-     * @return
+     *
+     * @param path         文件路径
+     * @param model        模型
+     * @return 视图
      */
     @GetMapping(value = "/openFile")
-    public String openFile(@RequestParam String path){
+    public String openFile(@RequestParam String path, Model model) {
         Process process;
         //自定义本地根目录(若想控制访问根目录，可在此设置)
         String directory = "";
         path = directory + path;
-        try {
-            process = new ProcessBuilder("open", path)
-                    .start();
-        } catch (IOException e) {
-            return e.getMessage();
-        }
-        try {
-            process.waitFor();
-        } catch (InterruptedException e) {
-            return e.getMessage();
-        }
-        process.destroy();
+        if (path.endsWith("xmind")) {
+            try {
 
-        return "closePage";
+                process = new ProcessBuilder("open", "-a", "xmind", path)
+                        .start();
+            } catch (IOException e) {
+                model.addAttribute("errorMessage", e.getMessage());
+                return errorPage;
+            }
+            try {
+                process.waitFor();
+            } catch (InterruptedException e) {
+                model.addAttribute("errorMessage", e.getMessage());
+                return errorPage;
+            }
+            process.destroy();
+
+            return closePage;
+        }
+        model.addAttribute("errorMessage", "不支持的文件类型");
+        return errorPage;
     }
 }
